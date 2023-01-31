9b558a3f158fc9a270681dfdfca68a91c568bb1c
qq5132834 <513283439@qq.com>
2023-01-31 14:04:38 +0800

    添加helloworld测试java、class、jimple文件。

--- sootOutput/HelloWorld/HelloWorld.java
+++ sootOutput/HelloWorld/HelloWorld.java
@@ -1,7 +1,19 @@
 //HelloWorld.java
 public class HelloWorld {
+
     public static void main(String[] args) {
-        System.out.println("hello");
+        int i = 1;
+        while (i <= 9) {
+            int j = 1;
+            while (j <= i) {
+                int sum = i * j;
+                System.out.print(j + "*" + i + "=" + sum + "  ");
+                j++;
+            }
+            i++;
+            System.out.println();
+        }
     }
+
 }
 
